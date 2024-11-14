27453cedf0b9807fe6827ad76b2e46397e0a4d7f
CristianDelgado31 <cristian-31-10@hotmail.com>
2024-11-14 17:18:05 -0300

    sprint 2:v4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,9 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environmentConfig } from './environments/environmentConfig';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp(environmentConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp(environmentConfig)), 
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),] // provideAnimationsAsync()
 };
